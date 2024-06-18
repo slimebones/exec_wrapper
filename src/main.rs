0898192cdd92e7baef5f41254ca8481908ca617b
ryzhovalex <thed4rkof@gmail.com>
2024-06-18 16:22:36 +0300

    update hello, hello.py, Cargo.lock, Cargo.toml, ...

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,65 @@
 /// Creates a wrapper executable for an action. Solves some nasty problem with
 /// calling non-executables on Windows from nushell terminal (when non-exe)
 /// files require extension in order to be called, which is not convenient).
+///
+/// Note that all commands are executed using "sh". For now it's hardcoded.
+use std::{
+    env::{self, current_dir, temp_dir},
+    fs,
+    process::Command,
+};
 
-use std::{env, fs, process::Command};
+static DEPLOY_STR: &str = r#"use std::process::Command;
+static CMD: &str = "REPLACE_TARGET";
+
+fn main() {
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(CMD)
+        .output()
+        .expect(format!("failed to run cmd {CMD}").as_str());
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    if !output.status.success() {
+        println!("status: {}", output.status);
+    }
+    if !stdout.trim().is_empty() {
+        println!("{}", stdout);
+    }
+    if !stderr.trim().is_empty() {
+        println!("{}", stderr);
+    }
+
+    assert!(output.status.success());
+}
+"#;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    assert!(args.len() == 2, "please provide only target cmd argument");
+    assert!(
+        args.len() == 3,
+        "please provide target cmd argument and output filename"
+    );
     let cmd = &args[1];
+    let out_filename = &args[2];
 
-    let deploy = fs::read_to_string(path)
+    let replaced_deploy = str::replace(&DEPLOY_STR, "REPLACE_TARGET", cmd);
+    let temp_dir = temp_dir();
+    fs::write(temp_dir.join("exec_wrapper_deploy.rs"), replaced_deploy)
+        .expect("unable to write file");
 
-    let output = Command::new("cargo")
-        .arg("--help")
+    let output = Command::new("rustc")
+        .current_dir(&temp_dir)
+        .arg("exec_wrapper_deploy.rs")
         .output()
-        .expect("fail to run cmd");
-    println!("status: {}", output.status);
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-
+        .expect("fail to compile deploy");
     assert!(output.status.success());
+
+    let current_dir = current_dir().unwrap();
+    fs::copy(
+        temp_dir.join("exec_wrapper_deploy.exe"),
+        current_dir.join(&out_filename),
+    )
+    .unwrap();
 }
